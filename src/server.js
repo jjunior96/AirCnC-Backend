eb16766e1dac609f8b2078baf0eb2c41e8005b7f
Junior Alves <jjalves96@gmail.com>
2019-10-03 08:12:42 -0300

    Feature Backend

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
+const path = require('path');
+
 // const Database = require('./database/index');
 const mongoose = require ('mongoose');
 
@@ -17,7 +20,10 @@ mongoConnection = mongoose.connect(
 
 // const bancoDeDados = Database;
 
+app.use(cors());
+
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 // app.use(bancoDeDados);
 app.use(routes);
 
